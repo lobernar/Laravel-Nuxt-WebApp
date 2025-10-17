8b02ecc65e8b8f07a674baaa0be2a1113da53860
Loic Bernard <loic.bernard@ulb.be>
2025-10-17 16:43:18 +0200

    Implemented user authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+// User authentication routes
+Route::post("/register", [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/logout', [UserController::class, 'logout']);
