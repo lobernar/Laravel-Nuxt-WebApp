51b7296134695c3ad7ac5c8804fbcc0b768ef615
Loic Bernard <loic.bernard@ulb.be>
2025-10-29 00:29:42 +0100

    Renamed test files

--- api/tests/Feature/AuthControllerTest.php
+++ api/tests/Feature/AuthControllerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class AuthTest extends TestCase
+class AuthControllerTest extends TestCase
 {
     /**
      * A basic feature test example.
