b8e3e329da498c57b63cbb50958f5499f2f20212
Loic Bernard <loic.bernard@ulb.be>
2025-10-29 13:53:14 +0100

    removed example route

--- api/routes/api.php
+++ api/routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\AdminController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 // Authentication routes
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
